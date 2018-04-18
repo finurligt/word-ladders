91ea8cf63eb7148e822f4255fc1227f17f3a48e5
André Hedesand <an7821he-s@student.lu.se>
2018-04-18 14:12:06 +0200

    Add simple input stream-parser

---
+++ src/Parser.java
@@ -0,0 +1,25 @@
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Parser {
+    public static void main(String[] args) {
+        String s = getStringFromStream(System.in);
+        System.out.println(s);
+    }
+
+    public static String getStringFromStream(InputStream is) {
+        Scanner sc = new Scanner(is);
+        List<String> lines = new LinkedList();
+
+        while(sc.hasNextLine()) {
+            lines.add(sc.nextLine());
+        }
+
+        Optional<String> result = lines.stream().reduce(String::concat);
+        return result.orElse("");
+    }
+
+}

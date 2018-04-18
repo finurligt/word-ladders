cc9a9229197445bcbe99c587e7954f6346792bc9
Fabian Sundholm <fabian.sundholm@gmail.com>
2018-04-18 14:14:28 +0200

    finnished algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,44 @@
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) {
         System.out.println("hej");
     }
-    public void findPath(Map<String,String[]> G, String s, String t) {
+    public void findPath(Map<String,String[]> neighborMap, String s, String t) {
         HashMap<String,String> pred = new HashMap<String,String>();
-        HashMap<String,Boolean> visited = new HashMap<String,Boolean>();
+        HashSet<String> visited = new HashSet<String>();
+
+        //q <- new list containing s
         Queue<String> q = new LinkedList<String>();
         q.add(s);
-        for (Map.Entry node: G.entrySet()) {
-            visited.put((String)node.getKey(), false);
-        }
-        visited.put(s,true);
 
+        //for v ∈ V visited(v) ← 0
+
+        //visited(s) ← 1
+        visited.add(s);
+
+        //while q 6= null
+        while(!q.isEmpty()) {
+            //v ← take out the first element from q
+            String currentNode = q.poll();
+            //for w ∈ neighbor(v)
+            for (String neighbor : neighborMap.get(currentNode)) {
+                //if not visited(w) then
+                if (!visited.contains(neighbor)) {
+                    //visited(w) ← 1
+                    visited.add(neighbor);
+                    //add w to end of q
+                    q.add(neighbor);
+                    //pred(w) ← v
+                    pred.put(neighbor,currentNode);
+                    //if w = t then
+                    if (neighbor.equals(t)) {
+                        System.out.println("found path s - t");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("found no path s - t");
     }
 }

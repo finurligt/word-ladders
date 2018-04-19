7cf786a6aa5577c9202992e5a44c72a52a56d52f
Fabian Sundholm <fabian.sundholm@gmail.com>
2018-04-19 14:40:43 +0200

    finnished solution

--- src/wordladders.java
+++ src/wordladders.java
@@ -1,8 +1,11 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-class Main {
+class wordladders {
 
     public static void main(String[] args) {
         if(args.length!=2) {
@@ -11,10 +14,53 @@ class Main {
             return;
         }
 
-        String wordList = args[0]
+        String wordList = args[0];
+        String pathList = args[1];
+
+
+        List<String> words;
+        List<String> path;
+        Map<String,List<String>> neighbors = new HashMap<>();
+        try {
+            words = getStringFromStream(new FileInputStream(wordList));
+            path = getStringFromStream(new FileInputStream(pathList));
+            for (String parent : words) {
+                neighbors.put(parent, new LinkedList<>());
+                for (String child : words) {
+                    if (shouldBeChildOf(parent,child)) {
+                        neighbors.get(parent).add(child);
+                    }
+                }
+            }
+
+            for (String row : path) {
+                String[] split = row.split(" ");
+                if(split[0].equals(split[1])) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(findPath(neighbors,split[0],split[1]));
+                }
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public int findPath(Map<String,String[]> neighborMap, String s, String t) {
+    public static List<String> getStringFromStream(InputStream is) {
+        Scanner sc = new Scanner(is);
+        LinkedList<String> lines = new LinkedList<>();
+
+        while(sc.hasNextLine()) {
+            lines.add(sc.nextLine());
+        }
+
+        return lines;
+
+    }
+
+    public static int findPath(Map<String,List<String>> neighborMap, String s, String t) {
         HashMap<String,String> pred = new HashMap<String,String>();
         HashSet<String> visited = new HashSet<String>();
 
@@ -54,10 +100,11 @@ class Main {
 
     }
 
-    private int backTrack(Map<String, String> pred, String s, String t)  {
+    private static int backTrack(Map<String, String> pred, String s, String t)  {
         int steps = 0;
         String currentNode = t;
         while (true) {
+
             if (pred.containsKey(currentNode)) {
                 currentNode = pred.get(currentNode);
                 steps++;
@@ -69,11 +116,13 @@ class Main {
     }
 
     public static boolean shouldBeChildOf(String p, String c) {
-
+        if (c.equals(p)) {
+            return false;
+        }
         String copyOfChild = String.valueOf(c);
 
         for(char ac : p.substring(1).toCharArray()) {
-            copyOfChild = copyOfChild.replace(String.valueOf(ac), "");
+            copyOfChild = copyOfChild.replaceFirst(String.valueOf(ac), "");
         }
 
         return copyOfChild.length() == 1;
